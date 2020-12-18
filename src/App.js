5fd6fa4d8bc5dc9162289fcf0733b4c3a3abec4f
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2020-12-18 21:45:14 +0300

    Project Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-export class App extends Component {
+import Layout from './components/Layout/Layout';
+
+class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+            <p>Yes</p>
+        </Layout>
       </div>
     )
   }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,10 @@ import Auxiliary from '../../hoc/Auxiliary';
 
 const layout=(props)=>(
 <Auxiliary>
-
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main>
+        {props.children}
+    </main>
 </Auxiliary>
 )
 
