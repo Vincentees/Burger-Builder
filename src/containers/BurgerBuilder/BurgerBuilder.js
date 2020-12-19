891439f82b41b58183392d6b4f69e9282666ace7
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2020-12-19 19:26:40 +0300

    Adding dynamic state to Burger Builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state={
+        ingredients:{
+            salad:1, 
+            bacon:1,
+            cheese:2,
+            meat:2
+        }
+    }
     render() {
         return (
         <Auxiliary>
-            <Burger></Burger>
+            <Burger ingredients={this.state.ingredients}></Burger>
             <div>Builder Controls</div>
         </Auxiliary>
                 
