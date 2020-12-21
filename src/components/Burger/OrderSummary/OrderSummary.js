e2490104f13bb2e8c109372f24e8f351d125407f
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2020-12-22 01:01:37 +0300

    Created order summary Modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Auxiliary from '../../../hoc/Auxiliary';
+
+const orderSummary=(props)=>{
+    const ingredientSummary=Object.keys(props.ingredients)
+    .map(igKey=>{
+        return (
+        <li key={igKey}>
+        <span style={{textTransform:'capitalize'}}>{igKey}:</span>{props.ingredients[igKey]}
+        </li>)
+    })
+return(
+    <Auxiliary>
+        <h3>Your order</h3>
+        <p>A decilious burger with the following ingredients:</p>
+        <ul>
+            {ingredientSummary}
+        </ul>
+        <p>Continue to Checkout?</p>
+    </Auxiliary>
+)
+}
+export default orderSummary;
