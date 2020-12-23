3d79361d722cb2b4e72ec8890b190845ef8084c3
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2020-12-23 06:56:32 +0300

    Added price to the Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,7 +17,7 @@ return(
         <ul>
             {ingredientSummary}
         </ul>
-        <p><strong>Total Price:{props.price}</strong></p>
+        <p><strong>Total Price:{props.price.toFixed(2)}</strong></p>
         <p>Continue to Checkout?</p>
         <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
         <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
