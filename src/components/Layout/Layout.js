2cfccada2ed63ff4bdbabf633376caffac6e1d3e
kipngetichvincent.k@gmail.com <kipngetichvincent.k@gmail.com>
2020-12-28 11:39:06 +0300

    created Responsive sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
 import Auxiliary from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout=(props)=>(
 <Auxiliary>
-    <Toolbar></Toolbar>
+    <Toolbar/>
+    <SideDrawer/>
     <main className={classes.Content}>
         {props.children}
     </main>
